29e9bec23b17caf02f1722eb6ae8a60ee89a22f2
BPkuma <omomotakuma@gmail.com>
2023-06-02 05:59:59 +0900

    welcome.blade.phpをランディングページに改造

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-guest-layout>
+    <div class="text-5xl text-center mt-6">マイポートフォリオ</div>
+    <div class="mt-14 grid grid-cols-3 gap-4 text-xl text-center text-neutral-50 mx-5">
+        <a href="{{ url('/fizzbuzz') }}" class="bg-blue-700 rounded-lg p-5 ">FizzBuzzクエスト</a>
+        <a href="{{ url('/summarize') }}" class="bg-blue-700 rounded-lg p-5 ">JavaScriptまとめ</a>
+        <a href="{{ url('/typing') }}" class="bg-blue-700 rounded-lg p-5 ">AnkiTyping</a>
+    </div>
+</x-guest-layout>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <style>
-
-        </style>
-    </head>
-    <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </div>
-        
-    </body>
-</html>
