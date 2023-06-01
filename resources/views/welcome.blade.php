b83bb49925d11eaccdbe68da49e0e2c97fcd0583
BPkuma <omomotakuma@gmail.com>
2023-06-02 06:33:17 +0900

    微調整

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="mt-14 grid grid-cols-3 gap-4 text-xl text-center text-neutral-50 mx-5">
         <a href="{{ url('/fizzbuzz') }}" class="bg-blue-700 rounded-lg p-5 ">FizzBuzzクエスト</a>
         <a href="{{ url('/summarize') }}" class="bg-blue-700 rounded-lg p-5 ">JavaScriptまとめ</a>
-        <a href="{{ url('/typing') }}" class="bg-blue-700 rounded-lg p-5 ">AnkiTyping</a>
+        <a href="{{ url('/typing') }}" class="bg-blue-700 rounded-lg p-5 ">Ankiタイピング</a>
     </div>
 </x-guest-layout>
 
