03193d908ceb1ecb1bf59c436cedd1a211c5a250
BPkuma <omomotakuma@gmail.com>
2023-06-02 07:38:46 +0900

    ランディングページをtailwindで遊んでみた

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,61 @@
 <x-guest-layout>
-    <div class="text-5xl text-center mt-6">マイポートフォリオ</div>
-    <div class="mt-14 grid grid-cols-3 gap-4 text-xl text-center text-neutral-50 mx-5">
-        <a href="{{ url('/fizzbuzz') }}" class="bg-blue-700 rounded-lg p-5 ">FizzBuzzクエスト</a>
-        <a href="{{ url('/summarize') }}" class="bg-blue-700 rounded-lg p-5 ">JavaScriptまとめ</a>
-        <a href="{{ url('/typing') }}" class="bg-blue-700 rounded-lg p-5 ">Ankiタイピング</a>
+    <div
+        class="
+        text-7xl text-center mt-6 p-5 font-bold
+        tracking-tight bg-gradient-to-r from-green-500 via-blue-500
+        to-pink-500 bg-clip-text text-transparent
+        ">
+    My Port Folio
     </div>
+
+    <div class="
+        mt-14 grid grid-cols-3 gap-4
+        text-xl text-center text-neutral-50
+        mx-5
+        ">
+        <a href="{{ url('/fizzbuzz') }}"
+            class="
+            bg-blue-700 rounded-lg p-5
+            hover:bg-blue-500 transition duration-300
+            ">
+        FizzBuzzクエスト
+        </a>
+        <a href="{{ url('/summarize') }}"
+            class="
+            bg-blue-700 rounded-lg p-5
+            hover:bg-blue-500 transition duration-300
+            ">
+        JavaScriptまとめ
+        </a>
+        <a href="{{ url('/typing') }}"
+            class="
+            bg-blue-700 rounded-lg p-5
+            hover:bg-blue-500 transition duration-300
+            ">
+        Ankiタイピング
+        </a>
+        <a href="{{ url('/typing') }}"
+            class="
+            bg-blue-700 rounded-lg p-5
+            hover:bg-blue-500 transition duration-300
+            ">
+        こんな漢字
+        </a>
+        <a href="{{ url('/typing') }}"
+            class="
+            bg-gray-700 rounded-lg p-5
+            hover:bg-gray-500 transition duration-300
+            ">
+        準備中...
+        </a>
+        <a href="{{ url('/typing') }}"
+            class="
+            bg-gray-700 rounded-lg p-5
+            hover:bg-gray-500 transition duration-300
+            ">
+        準備中...
+        </a>
+    </div>
+
 </x-guest-layout>
 
