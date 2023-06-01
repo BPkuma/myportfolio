52a3f579ec93dfa0706f82f99a3f5783faf3d150
BPkuma <omomotakuma@gmail.com>
2023-06-02 06:16:27 +0900

    ヘッダーのリンク関係を修正

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,7 +22,7 @@
                     @if (Route::has('login'))
                     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                         @auth
-                            <a href="{{ url('/post') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">HOME</a>
+                            <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">HOMEへ戻る</a>
                         @else
                             <a href="{{ route('login') }}" class="text-gray-700 dark:text-gray-500 underline font-bold text-base">ログイン</a>
 
