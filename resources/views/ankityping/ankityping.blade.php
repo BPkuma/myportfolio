cb6ed56c7610cd33293d563ebd99b09d30298d56
BPkuma <omomotakuma@gmail.com>
2023-06-03 23:26:15 +0900

    Ankiアプリの最初の画面にいい感じのコンポーネントを採用

--- resources/views/ankityping/ankityping.blade.php
+++ resources/views/ankityping/ankityping.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+
+
+
     <div
         class="
         text-7xl text-center mt-6 p-5 text-bold
@@ -7,31 +10,33 @@
     Anki Typing
     </div>
 
-    <div class="
-        mt-6 grid grid-cols-3 gap-4
-        text-xl text-center text-neutral-50
-        mx-5
-        ">
-            <a href="{{ url('/post') }}"
-                class="
-                bg-lime-700 rounded-lg p-5
-                hover:bg-lime-500 transition duration-1000
-                ">
-            問題を作る
-            </a>
-            <a href="{{ url('/typing/game') }}"
-                class="
-                bg-lime-700 rounded-lg p-5
-                hover:bg-lime-500 transition duration-1000
-                ">
-            ゲームで遊ぶ
-            </a>
-            <a href="{{ url('/typing') }}"
-                class="
-                bg-lime-700 rounded-lg p-5
-                hover:bg-lime-500 transition duration-1000
-                ">
-            設定
-            </a>
+    <div class="w-2/5 mx-auto mt-6 rounded-2xl bg-gradient-to-r from-lime-500 via-teal-500 to-cyan-500 p-1 shadow-2xl">
+        <a class="block rounded-xl bg-white p-4 sm:p-6 lg:p-8 hover:bg-gray-300 transition duration-1000" href="{{ url('/typing/game') }}">
+            <div class="mt-2">
+                <h3 class="text-xl font-bold text-gray-700 sm:text-xl">
+                    アプリで遊ぶ
+                </h3>
 
+                <p class="mt-2 text-base text-gray-500">
+                    問題文が表示され、伏せられた答えをタイピングします。<br>
+                    分からない時はエンターキーで１文字ずつ答えが現れます。
+                </p>
+            </div>
+        </a>
+    </div>
+    <div class="w-2/5 mx-auto mt-10 rounded-2xl bg-gradient-to-r from-lime-500 via-teal-500 to-cyan-500 p-1 shadow-2xl">
+        <a class="block rounded-xl bg-white p-4 sm:p-6 lg:p-8 hover:bg-gray-300 transition duration-1000" href="{{ url('/post') }}">
+            <div class="mt-2">
+                <h3 class="text-lg font-bold text-gray-700 sm:text-xl">
+                    問題を作る
+                </h3>
+
+                <p class="mt-2 text-base text-gray-500">
+                    問題文とその答えを自分で作り登録してください。<br>
+                    アプリではその中から出題されます。<br>
+                    答えには半角英数字にのみ対応しています。
+                </p>
+            </div>
+        </a>
+    </div>
 </x-app-layout>
--- resources/views/ankityping/typinggame.blade.php
+++ resources/views/ankityping/typinggame.blade.php
@@ -115,9 +115,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-body antialiased">
-        <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
@@ -126,155 +124,153 @@
                     </div>
                 </header>
             @endif
-
             <!-- Page Content -->
-            <main>
-                <div id="container">
-                    <div id="progress"></div>
-                    <div id="answer"></div>
-                    <div id="question"></div>
-                </div>
-                <div id="hint"></div>
-                <div id="message" class="fadein">エンターを押すと開始します</div>
-                <script>
-                    'use strict';
-                    {
-                    const matters = {!! json_encode($posts) !!};
-                    console.log(matters);
-                    let checkTexts = [];
-                    let container = document.getElementById('container');
-                    let answer = document.getElementById('answer');
-                    let question = document.getElementById('question');
-                    let hint = document.getElementById('hint');
-                    let progParent = document.getElementById('progress');
-                    let prog = document.createElement('progress');
-                    let message = document.getElementById('message');
-                    const amount = matters.length;
-                    let hit = 0;
-                    let count = 0;
-                    let state = false;
-                    let startTime;
+        <main>
+            <div id="container">
+                <div id="progress"></div>
+                <div id="answer"></div>
+                <div id="question"></div>
+            </div>
+            <div id="hint"></div>
+            <div id="message" class="fadein">エンターを押すと開始します</div>
+            <script>
+                'use strict';
+                {
+                const matters = {!! json_encode($posts) !!};
+                console.log(matters);
+                let checkTexts = [];
+                let container = document.getElementById('container');
+                let answer = document.getElementById('answer');
+                let question = document.getElementById('question');
+                let hint = document.getElementById('hint');
+                let progParent = document.getElementById('progress');
+                let prog = document.createElement('progress');
+                let message = document.getElementById('message');
+                const amount = matters.length;
+                let hit = 0;
+                let count = 0;
+                let state = false;
+                let startTime;
 
-                    function correctAnswer (e) {
-                    checkTexts[0].span.textContent = checkTexts[0].value;
-                    checkTexts[0].span.className = 'add-correct';
-                    hit++;
-                    checkTexts.shift();
-                    }
+                function correctAnswer (e) {
+                checkTexts[0].span.textContent = checkTexts[0].value;
+                checkTexts[0].span.className = 'add-correct';
+                hit++;
+                checkTexts.shift();
+                }
 
-                    function createText() {
-                    let rand = Math.floor(Math.random() * matters.length);
-                    let matter = matters.splice(rand, 1)[0];
-                    console.log(matter);
-                    progParent.appendChild(prog);
-                    prog.setAttribute("max", amount);
-                    prog.setAttribute("value", count);
+                function createText() {
+                let rand = Math.floor(Math.random() * matters.length);
+                let matter = matters.splice(rand, 1)[0];
+                console.log(matter);
+                progParent.appendChild(prog);
+                prog.setAttribute("max", amount);
+                prog.setAttribute("value", count);
 
-                    answer.textContent = '';
-                    question.innerText = matter.question;
+                answer.textContent = '';
+                question.innerText = matter.question;
 
-                    count++;
+                count++;
 
-                    // !..ここが一番のキモ 正解の文字列を分解して表示は＿でもvalueは正解の文字列というspan要素を作ってanswerノードの子要素に設置している
-                    checkTexts = matter.answer.split('').map(value => {
-                        let span = document.createElement('span');
-                        span.textContent = '_';
-                        answer.appendChild(span);
-                        return {span, value: value};
-                        });
+                // !..ここが一番のキモ 正解の文字列を分解して表示は＿でもvalueは正解の文字列というspan要素を作ってanswerノードの子要素に設置している
+                checkTexts = matter.answer.split('').map(value => {
+                    let span = document.createElement('span');
+                    span.textContent = '_';
+                    answer.appendChild(span);
+                    return {span, value: value};
+                    });
 
-                        hint.textContent = `最初の文字は ${checkTexts[0].value}`;
-                        setTimeout(() => {
-                        hint.classList.add('fadein')
-                    }, 4000);
-                    hint.classList.remove('fadein');
-                    }
+                    hint.textContent = `最初の文字は ${checkTexts[0].value}`;
+                    setTimeout(() => {
+                    hint.classList.add('fadein')
+                }, 4000);
+                hint.classList.remove('fadein');
+                }
 
-                    document.addEventListener('keydown', (e) => {
-                    if(e.code === 'Enter') {
-                        if(state === true) {
-                        correctAnswer(e);
+                document.addEventListener('keydown', (e) => {
+                if(e.code === 'Enter') {
+                    if(state === true) {
+                    correctAnswer(e);
 
-                        if(!checkTexts.length ){
-                            if(count >= amount) {
-                            const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2); // 経過時間を計算して小数点2位の文字列に変換
-                            const wpm = hit / elapsedTime * 60 // wpmを計算
-
-                            setTimeout(() => {
-                                answer.remove();
-                                question.remove();
-                                hint.remove();
-                                message.style.display = "";
-                                message.style.marginTop = "50px";
-                                message.innerText = `-------- RESULT --------\n回答数 : ${amount} 問\n入力文字数 : ${hit}\n経過時間 : ${elapsedTime} 秒\nWPM : ${wpm.toFixed(2)}\n\nエンターを押すと開始画面に移動します`;
-                            }, 400);
-
-                            document.addEventListener('keypress', e => {
-                                if(e.code === 'Enter') {
-                                window.location.reload();
-                                }
-                            });
-                            }
-                            setTimeout(() => {
-                            hint.classList.remove('fadein');
-                            prog.remove();
-                            container.classList.add('fadein');
-                            createText();
-                            }, 400);
-                        }
-                        return;
-                        }
-                        state = true;
-                        startTime = Date.now();
-                        question.style.background = "lightgray";
-                        message.textContent = "";
-                        message.style.display = "none";
-                        container.classList.add('fadein');
-                        createText();
-                    }
-
-                    // ユーザーの入力値(e.key)が正しいものではなかったら、文字を震わせるアニメーションclassをつけて、150ミリ秒後に外す
-                    if(checkTexts[0].value !== e.key) {
-                        checkTexts[0].span.classList.add('caution');
-                        setTimeout(() => {checkTexts[0].span.classList.remove('caution')},150);
-                        return;
-                    }
-                    if(checkTexts[0].value === e.key) {
-                        correctAnswer(e);
-
-                        //配列要素が空っぽになったら400ミリ秒後に次の問題を出す
-                        if(!checkTexts.length ){
+                    if(!checkTexts.length ){
                         if(count >= amount) {
-                            const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2); // 経過時間を計算して小数点2位の文字列に変換
-                            const wpm = hit / elapsedTime * 60 // wpmを計算
+                        const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2); // 経過時間を計算して小数点2位の文字列に変換
+                        const wpm = hit / elapsedTime * 60 // wpmを計算
 
-                            setTimeout(() => {
+                        setTimeout(() => {
                             answer.remove();
                             question.remove();
                             hint.remove();
                             message.style.display = "";
                             message.style.marginTop = "50px";
                             message.innerText = `-------- RESULT --------\n回答数 : ${amount} 問\n入力文字数 : ${hit}\n経過時間 : ${elapsedTime} 秒\nWPM : ${wpm.toFixed(2)}\n\nエンターを押すと開始画面に移動します`;
-                            }, 400);
+                        }, 400);
 
-                            document.addEventListener('keypress', e => {
+                        document.addEventListener('keypress', e => {
                             if(e.code === 'Enter') {
-                                window.location.reload();
+                            window.location.reload();
                             }
-                            });
+                        });
                         }
                         setTimeout(() => {
-                            hint.classList.remove('fadein');
-                            prog.remove();
-                            container.classList.add('fadein');
-                            createText();
+                        hint.classList.remove('fadein');
+                        prog.remove();
+                        container.classList.add('fadein');
+                        createText();
                         }, 400);
-                        }
                     }
-                    });
+                    return;
+                    }
+                    state = true;
+                    startTime = Date.now();
+                    question.style.background = "lightgray";
+                    message.textContent = "";
+                    message.style.display = "none";
+                    container.classList.add('fadein');
+                    createText();
                 }
-                </script>
-            </main>
-        </div>
+
+                // ユーザーの入力値(e.key)が正しいものではなかったら、文字を震わせるアニメーションclassをつけて、150ミリ秒後に外す
+                if(checkTexts[0].value !== e.key) {
+                    checkTexts[0].span.classList.add('caution');
+                    setTimeout(() => {checkTexts[0].span.classList.remove('caution')},150);
+                    return;
+                }
+                if(checkTexts[0].value === e.key) {
+                    correctAnswer(e);
+
+                    //配列要素が空っぽになったら400ミリ秒後に次の問題を出す
+                    if(!checkTexts.length ){
+                    if(count >= amount) {
+                        const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2); // 経過時間を計算して小数点2位の文字列に変換
+                        const wpm = hit / elapsedTime * 60 // wpmを計算
+
+                        setTimeout(() => {
+                        answer.remove();
+                        question.remove();
+                        hint.remove();
+                        message.style.display = "";
+                        message.style.marginTop = "50px";
+                        message.innerText = `-------- RESULT --------\n回答数 : ${amount} 問\n入力文字数 : ${hit}\n経過時間 : ${elapsedTime} 秒\nWPM : ${wpm.toFixed(2)}\n\nエンターを押すと開始画面に移動します`;
+                        }, 400);
+
+                        document.addEventListener('keypress', e => {
+                        if(e.code === 'Enter') {
+                            window.location.reload();
+                        }
+                        });
+                    }
+                    setTimeout(() => {
+                        hint.classList.remove('fadein');
+                        prog.remove();
+                        container.classList.add('fadein');
+                        createText();
+                    }, 400);
+                    }
+                }
+                });
+            }
+            </script>
+        </main>
     </body>
 </html>
