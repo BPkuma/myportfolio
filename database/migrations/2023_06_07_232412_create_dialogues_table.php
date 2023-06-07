87f499f5b60635f03f85754462a119e7901c8f0e
YurikoWatanabe <yuri0620yuri0620@gmail.com>
2023-06-08 03:06:45 +0900

    dialoguesテーブル作成

--- database/migrations/2023_06_07_232412_create_dialogues_table.php
+++ database/migrations/2023_06_07_232412_create_dialogues_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('dialogues', function (Blueprint $table) {
             $table->id();
             $table->integer('order');
-            $table->text('dialgue');
+            $table->text('dialogue');
             $table->timestamps();
         });
     }
