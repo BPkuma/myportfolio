1c1152455a812a67a6350462b57db18988688432
YurikoWatanabe <yuri0620yuri0620@gmail.com>
2023-06-09 02:47:58 +0900

    データベースに自由にレコードを挿入できるメソッド作成途中

--- app/Models/Dialogue.php
+++ app/Models/Dialogue.php
@@ -10,6 +10,44 @@ class Dialogue extends Model
     use HasFactory;
 
     //ホワイトリスト形式で保存可能なカラムを指定
-    protected $fillable = ['order', 'dialogue'];
+    protected $fillable = ['order', 'talk'];
 
+    //新しいレコードをまとめて追加するメソッド
+    public static function addTalks($talks) {
+        $existingTalks = Dialogue::whereIn('talk', $talks)->get();
+        foreach($existingTalks as $existingTalk) {
+            $existingTalk->delete();
+        }
+        Dialogue::whereNotIn('talk', $talks)->delete();
+
+        $max_order = Dialogue::max('order');
+        foreach($talks as $talk) {
+            $max_order++;
+            Dialogue::create([
+                'order' => $max_order,
+                'talk' => $talk
+            ]);
+        }
+    }
+
+    //新しいレコードを挿入する際、orderを振りなおすメソッド
+    public static function insertTalkAtOrder($talk, $order) {
+        $max_order = self::max('order');
+        $order = min($max_order + 1 , $order);
+        self::where('order', '>=', $order)->increment('order');
+        self::create([
+            'order' => $order,
+            'talk' => $talk
+        ]);  
+    }
+    //レコードを削除する際、orderを振りなおすメソッド
+    public static function deleteTalkAtOrder($order) {
+        self::where('order', $order)->delete();
+        self::where('order', '>', $order)->decrement('order');
+    }
+
+    //既存のレコードを更新するメソッド
+    public static function updateTalkAtOrder($order, $talk) {
+        self::where('order', $order)->update(['talk' => $talk]);
+    }
 }
