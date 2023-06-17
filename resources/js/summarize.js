cffbe09a79ae5b5192559081cee978e0dfbb7e5f
YurikoWatanabe <yuri0620yuri0620@gmail.com>
2023-06-18 05:06:44 +0900

    フラグの条件をファンクションに。スライムの表示条件調整中

--- resources/js/summarize.js
+++ resources/js/summarize.js
@@ -2,20 +2,25 @@
 //DOMが読み込まれたら////////////////////////////////////////////
 //document.addEventListener('DOMContentLoaded', () => {
     //スタート画面/////////////////////////////////////////////////
-    //▶カーソルを定数choose1,choose2,shoose3に代入
+    //▶カーソルを定数choose1,choose2,choose3,choose4に代入
     const choose1 = document.getElementById('choose1');
     const choose2 = document.getElementById('choose2');
-    const choose3= document.getElementById('choose3');
+    const choose3 = document.getElementById('choose3');
+    const choose4 = document.getElementById('choose4');
     //「おさらいする」を定数summarizeに代入
     const summarize = document.getElementById('summarize');
     //「ステータス確認」を定数stateに代入
     const state = document.getElementById('state');
     //「にげる」を定数runに代入
     const run = document.getElementById('run');
+    //「さんこうしりょう」を定数referenceに代入
+    const reference = document.getElementById('reference');
     //ステータスの中身を定数state_dataに代入
     const state_data = document.getElementById('state_data');
     //「にげる」クリック時の中身を定数run_dataに代入
-    const run_data = document.getElementById('run_data');
+    const run_data = document.getElementById('run_data');  
+    //「さんこうしりょう」クリック時の中身を定数reference-dataに代入
+    const reference_data = document.getElementById('reference_data');
     //マウスオーバーされた箇所の▶カーソルからhiddenを付けはずしするtoggleHidden()
     function toggleHidden(Show, ToHidden) { 
         //hidden削除 
@@ -29,14 +34,14 @@
     if (summarize) {
         //「おさらいする」にマウスオーバーされたら
         summarize.addEventListener('mouseover', function() {
-            toggleHidden(choose1, [choose2, choose3]);
+            toggleHidden(choose1, [choose2, choose3, choose4]);
         });
     }
     //stateが存在したら
     if (state) {
         //「ステータス確認」にマウスオーバーされたら
         state.addEventListener('mouseover', function() {
-            toggleHidden(choose2, [choose1, choose3]);
+            toggleHidden(choose2, [choose1, choose3, choose4]);
         });
         //「ステータス確認」にクリックされたら
         state.addEventListener('click', function() {
@@ -47,13 +52,25 @@
     if (run) {
         //「にげる」にマウスオーバーされたら
         run.addEventListener('mouseover', function() {
-            toggleHidden(choose3, [choose1, choose2]);
+            toggleHidden(choose3, [choose1, choose2, choose4]);
         });
         //「にげる」にクリックされたら
         run.addEventListener('click', function() {
             run_data.classList.toggle('hidden');
         });
     }
+    //freferenceが存在したら
+    if (reference) {
+        //「さんこうしりょう」にマウスオーバーされたら
+        reference.addEventListener('mouseover', function() {
+            toggleHidden(choose4, [choose1, choose2, choose3]);
+        });
+        //「さんこうしりょう」にクリックされたら
+        reference.addEventListener('click', function() {
+            reference_data.classList.toggle('hidden');
+        });
+    }
+
     //カウントダウンタイマーの表示////////////////////////////////////////    
     //spanタグのidを定数todayに代入
     const today = document.getElementById('today');
@@ -139,39 +156,53 @@
     //特定の画像を指定する用の変数indexを初期化
     let jasco_index = 0;
     let slime_index = 0;
+    let timer_id = null;
+    let interval_id = null;
     //ジャスコの画像を順番に表示させるShowImageJasco()作成
     function ShowImageJasco() {        
-        //全ての画像を取得し、定数imagesに代入
-        const images = document.querySelectorAll('img.jasco');    
-        //全ての画像にhidden追加
-        for (let i = 0; i < images.length; i++) {
-            images[i].classList.add('hidden');
+        //全てのジャスコ画像を取得し、定数jImagesに代入
+        const jImages = document.querySelectorAll('img.jasco');    
+        //全てのジャスコ画像にhidden追加
+        for (let i = 0; i < jImages.length; i++) {
+            jImages[i].classList.add('hidden');
         }   
         //画像数が存在したら   
-        if(images.length > 0) { 
+        if(jImages.length > 0) { 
             //jasco_index番目の画像からhidden削除 
-            images[jasco_index].classList.remove('hidden'); 
+            jImages[jasco_index].classList.remove('hidden'); 
             //jasco_indexに1を足し、画像数で割った余りを変数jasco_indexに代入   
-            jasco_index = (jasco_index + 1) % images.length;
+            jasco_index = (jasco_index + 1) % jImages.length;
         }  
     }
-    //スライムの画像を順番に表示させるShowImageslime()作成
-    function ShowImageslime() {        
-        //全ての画像を取得し、定数imagesに代入
-        const images = document.querySelectorAll('img.slime');    
-        //全ての画像にhidden追加
-        for (let i = 0; i < images.length; i++) {
-            images[i].classList.add('hidden');
-        }   
-        //画像数が存在したら   
-        if(images.length > 0) { 
+
+    //全てのスライム画像を取得し、定数sImagesに代入
+    const sImages = document.querySelectorAll('img.slime'); 
+    //スライムの画像を順番に表示させるShowImageSlime()作成
+    function ShowImageSlime() { 
+        //画像が存在したら
+        if(sImages.length > 0) {     
+            //全ての画像にhidden追加
+            for (let i = 0; i < sImages.length; i++) {
+                sImages[i].classList.add('hidden');
+            }            
             //slime_index番目の画像からhidden削除 
-            images[slime_index].classList.remove('hidden'); 
+            sImages[slime_index].classList.remove('hidden'); 
             //slime_indexに1を足し、画像数で割った余りを変数slime_indexに代入   
-            slime_index = (slime_index + 1) % images.length;
+            slime_index = (slime_index + 1) % sImages.length;
         }  
     }
-    //ShowImage()を繰り返し実行
+
+    //スライム表示を取り消すファンクション
+    function hideImageSlime() {
+        //画像が存在したら
+        if(sImages.length > 0) { 
+            //全ての画像にhidden追加
+            for (let i = 0; i < sImages.length; i++) {
+                sImages[i].classList.add('hidden');
+            }             
+        }
+    }
+    //ShowImageJasco()を繰り返し実行
     setInterval(ShowImageJasco, 1500);    
 
     //セリフの表示////////////////////////////////////////////////
@@ -183,6 +214,8 @@
     const confirm = document.querySelector('.confirm');
     //cursor-pointer要素を定数cursor_pointerに代入
     const cursor_pointer = document.querySelector('.cursor-pointer');
+    //back_button要素を定数back_buttonに代入
+    const back_button = document.querySelector('.back_button');
     //yesno要素を定数yesnoに代入
     const yesno = document.querySelector('.yesno');
     //yes・no要素を定数yes・noに代入
@@ -224,7 +257,97 @@
         } 
     }
     //実行
-    showDialogue(dialogues[current_index]);    
+    showDialogue(dialogues[current_index]);   
+    
+    //flagカラムの値によって表示・非表示をコントロールするファンクション
+    function handleDialogueFlag(flag) {
+        switch (flag) {
+            //フラグがuser_nameなら
+            case 'user_name':
+                let user_name = document.querySelector('.user_name').getAttribute('data-user-name');
+                let dialogue_username = dialogues[current_index].talk.replace('ななし', user_name);
+                talk.textContent = dialogue_username;
+                break;
+            //フラグがqなら、yesno選択肢表示
+            case 'q':
+                yesno.classList.remove('hidden');
+                confirm.classList.add('hidden');
+                break;
+            //フラグがslimeなら、スライム君登場
+            case 'slime':
+                
+                timer_id = setTimeout(ShowImageSlime, 300);
+                interval_id = setInterval(ShowImageSlime, 1500);
+                /* console.log(dialogues[current_index].order); */
+                /* if (dialogues[current_index] < 9) {           
+                    hideImageSlime();
+                } */
+                break;
+            //フラグがcode_nowなら、コードcode_now表示
+            case 'code_now':
+                code_now.classList.remove('hidden');
+                break;
+            //フラグがcode_goalなら、コードcode_goal表示
+            case 'code_goal':
+                code_now.classList.add('hidden');
+                code_goal.classList.remove('hidden');
+                break;
+            //フラグがcode_subtractionなら、コードcode_subtraction表示
+            case 'code_subtraction':
+                code_goal.classList.add('hidden');
+                code_subtraction.classList.remove('hidden');
+                break;
+            //フラグがcode_hmsなら、コードcode_hms表示
+            case 'code_hms':
+                code_subtraction.classList.add('hidden');
+                code_hms.classList.remove('hidden');
+                break;
+            //フラグがcode_countdownなら、コードcode_countdown表示
+            case 'code_countdown':
+                code_hms.classList.add('hidden');
+                code_countdown.classList.remove('hidden');
+                break;
+            //フラグがcode_backquoteなら、コードcode_backquote表示
+            case 'code_backquote':
+                code_countdown.classList.add('hidden');
+                code_backquote.classList.remove('hidden');
+                break;
+            //フラグがcode_htmlなら、コードcode_html表示
+            case 'code_html':
+                code_backquote.classList.add('hidden');
+                code_html.classList.remove('hidden');
+                break;
+            //フラグがcode_rewriteなら、コードcode_rewrite表示
+            case 'code_rewrite':
+                code_rewrite.classList.remove('hidden');
+                break;
+            //フラグがsampleなら、サンプルsample表示
+            case 'sample':
+                code_html.classList.add('hidden');
+                code_rewrite.classList.add('hidden');
+                sample.classList.remove('hidden');
+                break;
+            //フラグがcode_recalcなら、コードcode_recalc表示
+            case 'code_recalc':
+                code_recalc.classList.remove('hidden');
+                break;
+            //フラグがsample2なら、サンプルsample2表示
+            case 'sample2':                        
+                code_recalc.classList.add('hidden');
+                sample2.classList.remove('hidden');                        
+                break;
+            //フラグがcode_padstartなら、サンプルcode_padstart表示
+            case 'code_padstart':
+                code_padstart.classList.remove('hidden');
+                break;
+            //フラグがsample3なら、サンプルsample3表示
+            case 'sample3':
+                code_padstart.classList.add('hidden');
+                sample3.classList.remove('hidden');
+                break;           
+
+        }
+    }
 
     //confirmがnullではない場合
     if (confirm !== null) {
@@ -232,91 +355,45 @@
         cursor_pointer.addEventListener('click', function() {    
             //current_indexがオブジェクトの個数より少なかったら
             if(current_index  < dialogues.length) {
+                if(current_index <= 0) {
+                    showDialogue(dialogues[0]);
+                    current_index++;
+                }  else {
+                    showDialogue(dialogues[current_index]); 
                 //showDialogue()実行
-                showDialogue(dialogues[current_index]); 
-                //会話内容にフラグがあった場合 
-                switch(dialogues[current_index].flag) {
-                    //フラグがqなら、yesno選択肢表示
-                    case 'q':
-                        yesno.classList.remove('hidden');
-                        confirm.classList.add('hidden');
-                        break;
-                    //フラグがslimeなら、スライム君登場
-                    case 'slime':
-                        setTimeout(ShowImageslime, 300);
-                        setInterval(ShowImageslime, 1500);  
-                        break;
-                    //フラグがcode_nowなら、コードcode_now表示
-                    case 'code_now':
-                        code_now.classList.remove('hidden');
-                        break;
-                    //フラグがcode_goalなら、コードcode_goal表示
-                    case 'code_goal':
-                        code_now.classList.add('hidden');
-                        code_goal.classList.remove('hidden');
-                        break;
-                    //フラグがcode_subtractionなら、コードcode_subtraction表示
-                    case 'code_subtraction':
-                        code_goal.classList.add('hidden');
-                        code_subtraction.classList.remove('hidden');
-                        break;
-                    //フラグがcode_hmsなら、コードcode_hms表示
-                    case 'code_hms':
-                        code_subtraction.classList.add('hidden');
-                        code_hms.classList.remove('hidden');
-                        break;
-                    //フラグがcode_countdownなら、コードcode_countdown表示
-                    case 'code_countdown':
-                        code_hms.classList.add('hidden');
-                        code_countdown.classList.remove('hidden');
-                        break;
-                    //フラグがcode_backquoteなら、コードcode_backquote表示
-                    case 'code_backquote':
-                        code_countdown.classList.add('hidden');
-                        code_backquote.classList.remove('hidden');
-                        break;
-                    //フラグがcode_htmlなら、コードcode_html表示
-                    case 'code_html':
-                        code_backquote.classList.add('hidden');
-                        code_html.classList.remove('hidden');
-                        break;
-                    //フラグがcode_rewriteなら、コードcode_rewrite表示
-                    case 'code_rewrite':
-                        code_rewrite.classList.remove('hidden');
-                        break;
-                    //フラグがsampleなら、サンプルsample表示
-                    case 'sample':
-                        code_html.classList.add('hidden');
-                        code_rewrite.classList.add('hidden');
-                        sample.classList.remove('hidden');
-                        break;
-                    //フラグがcode_recalcなら、コードcode_recalc表示
-                    case 'code_recalc':
-                        code_recalc.classList.remove('hidden');
-                        break;
-                    //フラグがsample2なら、サンプルsample2表示
-                    case 'sample2':
-                        code_recalc.classList.add('hidden');
-                        sample.classList.add('hidden');
-                        sample2.classList.remove('hidden');
-                        break;
-                    //フラグがcode_padstartなら、サンプルcode_padstart表示
-                    case 'code_padstart':
-                        code_padstart.classList.remove('hidden');
-                        break;
-                    //フラグがsample3なら、サンプルsample3表示
-                    case 'sample3':
-                        sample2.classList.add('hidden');
-                        code_padstart.classList.add('hidden');
-                        sample3.classList.remove('hidden');
-                        break;
+                showDialogue(dialogues[current_index]);
                 }
+                //会話内容にフラグがあった場合 
+                handleDialogueFlag(dialogues[current_index].flag);                
                 //インクリメント
-                current_index++;                        
-            } else {
-                console.log('表示終わり');
-            }
-            
+               current_index++;           
+
+            }  //最後のページだったら、トップ画面に遷移させる
+            else if (current_index == dialogues.length) {
+                window.location.href = '/summarize';
+            }            
+        });
+        //back_buttonがクリックされたら
+        back_button.addEventListener('click', function() {
+            if(current_index < dialogues.length) {
+                if (current_index <= 9) {
+                    hideImageSlime();
+                    clearTimeout(timer_id);            
+                    clearInterval(interval_id);
+                    timer_id = null;
+                    interval_id = null;                    
+                }  
+                if(current_index <= 0) {
+                    showDialogue(dialogues[0]);
+                } else {
+                    showDialogue(dialogues[current_index]); 
+                }
+                //会話内容にフラグがあった場合
+                handleDialogueFlag(dialogues[current_index].flag);                
+                //デクリメント
+               current_index--;
+            } 
+                      
         });
     }     
 
@@ -336,17 +413,28 @@
             yesno.classList.add('hidden');
             confirm.classList.remove('hidden');
         });
+        //back_buttonがクリックされたら
+        back_button.addEventListener('click', function() {
+            showDialogue(dialogues[current_index]);;
+            yesno.classList.add('hidden');
+            confirm.classList.remove('hidden');
+            //フラグチェックする
+            handleDialogueFlag(dialogues[current_index].flag); 
+        });
         
         //noがクリックされたら
-        no.addEventListener('click', function() {
+        no.addEventListener('click', function() {  
+            //固定セリフ          
             forever();
-            
+            //会話ループのための定数loop_index定義
+            const loop_index = current_index - 1;
             setTimeout(function() {
-                showDialogue(dialogues[current_index -1]);
-            }, 2000);   
-            yesno.classList.remove('hidden');         
+                showDialogue(dialogues[loop_index]);
+            }, 1000);   
+            yesno.classList.remove('hidden');                    
         });
 
+        //no選択時の固定セリフ
         function forever() {
             talk.textContent = 'ジャスコ「え？よくきこえませんでした。」';
             yesno.classList.add('hidden');
