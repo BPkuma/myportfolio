d3450acc309c0299f043454fde33d697953ce58a
BPkuma <omomotakuma@gmail.com>
2023-06-24 07:57:47 +0900

    ankityping.balade.phpで、ブラウザ幅が一定以上にあるとbuttonタグの幅がそれ以上広がらなくなる不具合修正

--- resources/views/ankityping/ankityping.blade.php
+++ resources/views/ankityping/ankityping.blade.php
@@ -4,13 +4,14 @@
     </div>
 <form method="POST" action="{{ route('typing.game', ['amount' => $amount]) }}">
     @csrf
-    <div class="w-2/5 mx-auto mt-6 rounded-2xl bg-gradient-to-r from-lime-500 via-teal-500 to-cyan-500 p-1 shadow-2xl">
-        <button type="submit" class="text-left block rounded-xl bg-white p-4 sm:p-6 lg:p-8 hover:bg-gray-200 transition duration-1000">
+    <div class="w-2/5 mx-auto mt-10 rounded-2xl bg-gradient-to-r
+                first-letter: from-lime-500 via-teal-500 to-cyan-500
+                p-1 shadow-2xl">
+        <button type="submit" class="w-full block text-left rounded-xl bg-white p-4 sm:p-6 lg:p-8 hover:bg-gray-200 transition duration-1000">
             <div class="mt-2">
-                <h3 class="text-xl font-bold text-gray-700 sm:text-xl">
+                <h3 class="text-lg font-bold text-gray-700 sm:text-xl">
                     アプリで遊ぶ
                 </h3>
-
                 <p class="mt-2 text-base text-gray-500">
                     問題文が表示され、伏せられた答えをタイピングします。<br>
                     分からない時はエンターキーで１文字ずつ答えが現れます。
